8ff96b002b8be7055c162fe0af49f9d30d3d3ead
Martin Johannes Rønnekjær Jensen <martin.jensen.1997@hotmail.com>
2023-12-04 21:07:20 +0100

    simplify

--- src/AngularApp.jsx
+++ src/AngularApp.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect } from 'react';
 
 function AngularApp() {
   const appRef = useRef(null);
@@ -6,23 +6,23 @@ function AngularApp() {
   // https://stackoverflow.com/questions/34424845/adding-script-tag-to-react-jsx
   useEffect(() => {
     const script = document.createElement('script');
-  
-    script.src = "/src/app.module.js";
-    script.type = "module";
+
+    script.src = '/src/app.module.js';
+    script.type = 'module';
     script.async = true;
-  
+
     appRef.current.appendChild(script);
-  
+
     return () => {
       appRef.current.removeChild(script);
-    }
+    };
   }, []);
 
   return (
-    <div ref={appRef} ng-app="app">
-      <span ng-controller="HelloWorldCtrl" dangerouslySetInnerHTML={{ __html: "{{message}}"}}></span>
+    <div ref={appRef} ng-app='app'>
+      <span ng-controller='HelloWorldCtrl'>{'{{message}}'}</span>
     </div>
-  )
+  );
 }
 
-export default AngularApp
+export default AngularApp;
